24b49b86722afaab405225767f2b44a641e39712
Volodymyr Pavlyk <vv.dr.pvlk@gmail.com>
2021-08-15 19:27:47 +0300

    created validation form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Switch, Route, Link} from 'react-router-dom';
+import SignUp from  './components/SignUp';
+import SignIn from './components/SignIn';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+      <Link className="link link_button" to="/sign-in">Sign In</Link>
+      <Link className="link link_button" to="/sign-up">Sign Up</Link>
+      
+      <Switch>
+        <Route path="/sign-in" component={SignIn}/>
+        <Route path="/sign-up" component={SignUp}/>
+      </Switch>
     </div>
   );
 }
